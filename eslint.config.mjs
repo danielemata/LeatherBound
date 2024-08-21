ca47acf70186510fb93a97f8b0882d1560cf6aa1
danielemata <danielemata@gmail.com>
2024-08-21 16:01:25 -0500

    Remove unnecessary Prettier import in ESLint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,6 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "@typescript-eslint/eslint-plugin";
 import tsParser from "@typescript-eslint/parser";
-import prettier from "eslint-config-prettier";
 import prettierPlugin from "eslint-plugin-prettier";
 
 export default [
